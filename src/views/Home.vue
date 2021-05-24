078c070eedaa0f66a564446bc616748cc29c114c
Fran249 <Elsilencio_F@hotmail.com>
2021-05-24 20:41:03 -0300

    Summary1(Unico)
    Calendario Dolar, consumiendo API

---
+++ src/views/Home.vue
@@ -0,0 +1,75 @@
+<template>
+  <div>
+    <v-layout :wrap="true">
+      <v-flex xs12>
+        <v-card>
+          <v-date-picker
+          v-model="fecha"
+          full-width
+          locale="es-cl"
+          :min="minimo"
+          :max="maximo"
+          @change="getDolar(fecha)">
+
+          </v-date-picker>
+        </v-card>
+        <v-card color="error" dark>
+          <v-card-text class="display-1 text-center">
+           {{valor}}
+          </v-card-text>
+        </v-card>
+      </v-flex>
+    </v-layout>
+  </div>
+</template>
+
+<script>
+  import axios from "axios";
+  import { mapMutations } from "vuex";
+
+  export default {
+    data(){
+      return{
+        fecha: new Date().toISOString().substr(0, 10),
+        minimo: '1984',
+        maximo: new Date().toISOString().substr(0, 10),
+        valor: null,
+      }
+    },
+    methods:{
+
+      ...mapMutations(['mostrarLoading', 'ocultarLoading']),
+
+      async getDolar(dia){
+        // console.log(dia);
+        let arrayFecha = dia.split(['-'])
+        let ddmmyy = arrayFecha[2]+ '-' + arrayFecha[1] + '-' + arrayFecha[0];
+        try{
+
+          this.mostrarLoading({titulo:'Accediendo a Informacion', color: 'secondary'})
+
+          let datos = await axios.get(`https://mindicador.cl/api/dolar/${ddmmyy}`);
+          
+          if(datos.data.serie.lenght > 0) {
+            this.valor = await datos.data.serie[0].valor
+          }else{
+            this.valor = 'Sin resultados'
+          }
+
+          // console.log(datos.data);
+
+          this.valor = await datos.data.serie[0].valor;
+
+        } catch (error) {
+          // console.log(error);
+        }
+        finally{
+          this.ocultarLoading();
+        }
+      }
+    },
+    created(){
+      this.getDolar(this.fecha)
+    }
+  }
+</script>
